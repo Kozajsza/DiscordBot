982eda81507deab5907629b6fc2cf7a84c7eb931
MKoz <34948831+Kozajsza@users.noreply.github.com>
2023-03-15 16:37:42 +0000

    added davinci-03 and waste.txt

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,11 @@ intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix='?', intents=intents)
 client = commands.Bot(command_prefix='?', intents=intents)
-openai.api_key = "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"
+openai.api_key = "OpenAI_API"
+
+# Load the content of waste.txt
+with open("waste.txt", "r", encoding="utf-8") as f:
+    waste_content = f.read()
 
 @client.event
 async def on_ready():
@@ -16,7 +20,7 @@ async def on_ready():
 @client.command()
 async def chat(ctx, *, message):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=message,
         temperature=0.5,
         max_tokens=1024,
@@ -27,7 +31,22 @@ async def chat(ctx, *, message):
     response_text = response.choices[0].text[:2000]  # Truncate response text to 2000 characters
     await ctx.send(response_text)
 
+@client.command()
+async def chat_fm(ctx, *, message):
+    # Combine the input message with the content of waste.txt
+    prompt = f"{message}\n\n{waste_content}"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        timeout=15,
+    )
+    response_text = response.choices[0].text[:2000]  # Truncate response text to 2000 characters
+    await ctx.send(response_text)
 
 
-BotToken = "MTA4NTUyMTI0NjY5NzA0MjAwMg.GqHnAE.Z1_ijK3KnsUeFQFZaPY9W6CXtkwguAPY1rtZVY"
+BotToken = "Discord_API"
 client.run(BotToken)
